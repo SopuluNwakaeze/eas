67935cae92a6d152c24ed39894079701fb553cc9
SopuluNwakaeze <sopnwakaeze@gmail.com>
2024-11-08 12:57:18 -0700

    Added Reset Button, and Hover Functionality
    I added a reset button so that the user could reset their drawing without reloading the page
    I added the hover functionality that allows the user to create drawings

--- script/index-script.js
+++ script/index-script.js
@@ -1,4 +1,5 @@
 const masterDiv = document.querySelector("#master-div");
+const resetButton = document.querySelector(".reset-button");
 
 for(var j = 0; j < 16; j++){
     const rowDiv = document.createElement("div");
@@ -6,8 +7,21 @@ for(var j = 0; j < 16; j++){
     for(var i = 0; i < 16; i++){
         const div = document.createElement("div");
         rowDiv.appendChild(div);
-        div.textContent = "test";
+        //div.textContent = i;
         div.setAttribute("class", "cells");
     }
     masterDiv.appendChild(rowDiv)
 }
+
+const divs = document.querySelectorAll(".cells")
+divs.forEach((div) => {div.addEventListener("mouseover", function(e){
+    div.setAttribute("class", "hovered-cell")
+});
+})
+
+resetButton.addEventListener("click", function(e){
+    console.log(e);
+    divs.forEach((div) => {div.setAttribute("class","cells")});
+});
+
+
